390f24e946017d6d9d3fb1ef42bc8e14f4739b6a
pleasesavemyearth <qkrwlgus0317@naver.com>
2021-12-28 11:10:59 +0900

    commit

--- LoginProcess.php
+++ LoginProcess.php
@@ -1,12 +1,12 @@
 <?php
 // 1. Get the value from variable
-$username = $_POST['userid'];
+$userid = $_POST['userid']; 
 $password = $_POST['userpwd'];
 //$php의 변수 = $_POST['html의 변수'] (LoginForm의 input태그의 name)
 
 
 // 2. 아이디 또는 비밀번호 중 하나라도 입력하지 않으면 LoginForm으로 redirection
-if(empty($userid) || empty($userpwd) ) {
+if(empty($userid) || empty($password) ) {
     header('Location: LoginSuccess.php'); 
 }
 
@@ -28,6 +28,8 @@ $sql = "SELECT userpwd FROM jh_user WHERE userid = '".$userid."'";
 
 
 // 5. running and return result !!db에 레코드 등록하고 로그인 하는데 성공페이지로 안넘어간다 해결..2번의 location위치 오타나서 안됐었음;
+// 비밀번호가 틀렸습니다 안뜸, loginprocess로 넘어감
+// 그냥 로그인 눌러도 로그인이 성공함
 $resultset = mysqli_query($dbconn, $sql);
 
 while($row = mysqli_fetch_array($resultset)) {
--- RegistProcess.php
+++ RegistProcess.php
@@ -1,10 +1,13 @@
 <?php
 //1. Get value from variable, POST method
+$userid = $_POST['userid'];
+$password = $_POST['userpwd'];
 $username = $_POST['username'];
-$password = $_POST['password'];
+$useremail = $_POST['useremail'];
+$usercellphone = $_POST['usercellphone'];
 
 //2.  Form validation 
-if(empty($username) || empty($password)){
+if(empty($userid) || empty($password)){
     header('Location: RegistForm.php');
   }
 
@@ -16,29 +19,32 @@ $db = 'webdb';
 
 $dbconn =  mysqli_connect($hostname, $user, $pass, $db);
 
-//4. 
-$sql = "SELECT * FROM jh_user WHERE userid='". $username."' ";
+//4. 중복체크를 위한 질의 구성
+$sql = "SELECT * FROM jh_user WHERE userid='". $userid . "' ";
 
-// 5.
+// 5. 중복체크를 위한 질의 실행
 $resultset = mysqli_query($dbconn, $sql); 
 $number = mysqli_num_rows($resultset); //resultset안에 몇개의 레코드가 있는 지 숫자로 반환
 
-//6.1 
+//6.1 중복 계정이 있으면 회원가입 폼으로 이동
 if ($number > 0) {
     header('Location: RegistForm.php');
   } else {
 
-//7. sql 구성
-$sql = "INSERT INTO jh_user(userid, userpwd) VALUES('". $username . "','". $password ."')";
+// 6.2 없으면 다음 단계로 진행
 
-//8. 
+//7. sql 구성 !반점 하나 빠져서 db등록이 안됬었음..
+$sql = "INSERT INTO jh_user(userid, userpwd, username, useremail, usercellphone) VALUES('". $userid . "','". $password ."', '". $username . "', '" . $useremail . "', '" . $usercellphone . "')";
+
+//8. 질의어 실행
 $result = mysqli_query($dbconn, $sql);
 
-// 9.
+// 9. login폼으로 이동
 if ($result) {
     header('Location: LoginForm.php');
   } 
 
   } 
 // !! 회원가입을 누르면 자꾸 loginsuccess.php로 넘어가는 이유 : registform의 form태그의 action을 loginprocess로 받아주고 있었음..;;
+// !!! 회원가입시 db에 id만 등록이 되고 나머진 안됨
 ?>
